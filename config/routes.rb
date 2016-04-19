1973b09e4634fcf4b5b51772b13971e88ab3546d
Sean Haughey <sdhaughey@gmail.com>
2016-04-19 09:42:16 -0500

    created search options form on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :businesses
-  root 'businesses#index'
+  resources :home
+  root 'home#index'
+
+  get '/id' => 'home#id_search'
+  get '/page' => 'home#page_search'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
