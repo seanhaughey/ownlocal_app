4645140ed96930aadaa0c781f645f8929343e4ab
Sean Haughey <sdhaughey@gmail.com>
2016-04-19 15:39:52 -0500

    added get all test

--- spec/views/businesses/index.html.erb_spec.rb
+++ spec/views/businesses/index.html.erb_spec.rb
@@ -34,13 +34,13 @@ RSpec.describe "businesses/index", type: :view do
     render
     assert_select "tr>td", :text => "Uuid".to_s, :count => 2
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 6
+    assert_select "tr>td", :text => "MyText".to_s, :count => 6
     assert_select "tr>td", :text => "City".to_s, :count => 2
     assert_select "tr>td", :text => "State".to_s, :count => 2
     assert_select "tr>td", :text => "Zip".to_s, :count => 2
     assert_select "tr>td", :text => "Country".to_s, :count => 2
     assert_select "tr>td", :text => "Phone".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 6
   end
 end
